60e475b466e82d4ab0792c043e80342110a1d0c8
Tim <timmyway1990@gmail.com>
2023-12-22 20:04:56 +0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from "react-bootstrap/Container";
+import Header from './components/Header';
+export default function App() {  
+  const workouts = [
+    {
+      id: 1,
+      name: 'Bench Press',
+      reps: 12,
+      series: 5,
+      type: 'dynamic',
+      time: null,
+      load: 50,
+      createdAt: 'today'
+    },
+    {
+      id: 2,
+      name: 'Pull up',
+      reps: 15,
+      series: 4,
+      type: 'dynamic',
+      time: null,      
+      load: null,
+      createdAt: 'today'
+    },
+    {
+      id: 3,
+      name: 'Front Lever hold',
+      reps: 15,
+      type: 'static',
+      time: 30,
+      series: 4,
+      load: null,
+      createdAt: 'today'
+    },
+    {
+      id: 4,
+      name: 'Vertical row',
+      reps: 15,
+      series: 5,
+      type: 'dynamic',
+      time: null,      
+      load: null,
+      createdAt: 'today'
+    },
+    {
+      id: 5,
+      name: 'Weighted Pull up',
+      reps: 5,
+      series: 5,
+      load: 20,
+      type: 'dynamic',
+      time: null,      
+      createdAt: 'today'
+    }
+  ];  
 
-function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container fluid>
+    <Header />
+    {workouts.length === 0 ?
+      <p>No workout found</p>
+    :
+      <>
+      {workouts.map(workout => {
+        return (
+          <>
+          <div key={workout.id} className='d-flex gap-2'>
+            <p>
+              {workout.id}
+            </p>
+            <p>
+              {workout.name}
+            </p>
+          </div>
+          </>
+        )
+      })}
+      </>
+    }    
+    </Container>
   );
 }
-
-export default App;
